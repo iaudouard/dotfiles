f445f0d737da6c0c66ab17891cb6b9d0829d0e25
iaudouard <i.audouard22@ejm.org>
2025-12-30 16:21:51 -0500

    feat(nvim): added oil toggle keymap

--- nvim/.config/nvim/lua/ivanaudouard/plugins/oil.lua
+++ nvim/.config/nvim/lua/ivanaudouard/plugins/oil.lua
@@ -5,4 +5,18 @@ return {
   opts = {},
   dependencies = { { 'nvim-mini/mini.icons', opts = {} } },
   lazy = false,
+  config = function()
+    require('oil').setup()
+    local function toggle_oil()
+      local oil = require 'oil'
+      -- if in an oil buffer, close it
+      if vim.bo.filetype == 'oil' then
+        oil.close()
+      else
+        oil.open()
+      end
+    end
+
+    vim.keymap.set('n', '<leader>e', toggle_oil, { desc = 'Toggle Oil file explorer' })
+  end,
 }
