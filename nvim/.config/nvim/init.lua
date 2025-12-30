c0ee103664dc16e478003f2f173016251bb2061c
iaudouard <i.audouard22@ejm.org>
2025-12-30 16:08:18 -0500

    feat(nvim): removed neo tree and added oil.nvim

--- nvim/.config/nvim/init.lua
+++ nvim/.config/nvim/init.lua
@@ -825,7 +825,7 @@ require('lazy').setup({
   require 'kickstart.plugins.indent_line',
   require 'kickstart.plugins.lint',
   require 'kickstart.plugins.autopairs',
-  require 'kickstart.plugins.neo-tree',
+  -- require 'kickstart.plugins.neo-tree',
   require 'kickstart.plugins.gitsigns',
 
   -- Custom plugins
